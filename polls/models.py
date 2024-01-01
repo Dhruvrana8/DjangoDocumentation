c1d61fb5a399f4c18293b1428ea60ae0aac99913
Dhruv Rana <dhruvrana4@gmail.com>
2024-01-01 21:25:20 +0530

    refactor: pub_date in the Question model

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField("Date Published")
+    pub_date = models.DateTimeField("Date Published")
+    
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choices(models.Model):
     question=models.ForeignKey(Question,on_delete = models.CASCADE)
     choices_text=models.CharField(max_length=200)
     vote= models.IntegerField(default = 0)
+    def __str__(self):
+        return self.question_text
+    
